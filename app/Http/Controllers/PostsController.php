0e1d5ff8dc23291305e368f15ae565c633c80154
David <david.grech26@gmail.com>
2020-11-02 18:52:18 +0000

    Add Laravel Collective HTML

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use DB;
 
 class PostsController extends Controller
 {
@@ -15,7 +16,11 @@ class PostsController extends Controller
     public function index()
     {
         
-        $posts = Post::all();
+        //$posts = Post::all();
+        //Eloquent database query
+        //$posts = Post::orderBy('title', 'asc')->get();
+        //Raw database query
+        $posts = DB::select('SELECT * FROM posts');
         return view('posts.index')->with('posts', $posts);
     }
 
@@ -26,7 +31,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
@@ -48,7 +53,8 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.show')->with('post', $post);
     }
 
     /**
