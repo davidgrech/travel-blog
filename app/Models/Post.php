851caf0b11fdc20bb156896d9c2f08a5bc022d82
David <david.grech26@gmail.com>
2020-11-02 14:29:35 +0000

    Add post controller

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    //table name
+    //protected $table = 'posts';
+    //primary key
+    //public $primaryKey = 'item_id';
+    //timestamps
+    //public $timestamp = false;
+
 }
