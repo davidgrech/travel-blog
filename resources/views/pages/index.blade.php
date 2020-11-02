12de5c0561ffcafb0a6300fcd7123ecac36aca91
David <david.grech26@gmail.com>
2020-11-02 11:41:10 +0000

    Add Laravel UI Project

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
       <h1>{{$title}}</h1>
-      <p>This is the about page</p>
+      <p>This is the index page</p>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/hello', function () {
-  return '<h1>Hello World</h1>';
-});
+Route::get('/', 'App\Http\Controllers\PagesController@index');
 
-Route::get('/about', function () {
-  return view('pages.about');
-});
+Route::get('/about', 'App\Http\Controllers\PagesController@about');
+
+Route::get('/services', 'App\Http\Controllers\PagesController@services');
+
+
+
+
+// Route::get('/hello', function () {
+//   return '<h1>Hello World</h1>';
+// });
+
+// Route::get('/about', function () {
+//   return view('pages.about');
+// });
 
 Route::get('/users/{id}', function ($id) {
   return 'this is a user ' . $id;
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
