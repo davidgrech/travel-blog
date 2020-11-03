8e3038a753bc2a1de7f146fce359d1a93e39261b
David <david.grech26@gmail.com>
2020-11-03 17:07:49 +0000

    Add edit.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,9 +2,10 @@
 
 @section('content')
   <a href="/posts" class="btn btn-default">Go Back</a>
- <h1>{{$post->title}}</h1>
- <p>{{$post->body}}</p>
- <hr>
- <small>Written on {{$post->created_at}}</small>
-
+  <h1>{{$post->title}}</h1>
+  <p>{{$post->body}}</p>
+  <hr>
+  <small>Written on {{$post->created_at}}</small>
+  <hr>
+  <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
 @endsection
