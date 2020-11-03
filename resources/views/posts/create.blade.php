8ffe3f740834890ccc26aedb3bd9dadfc504af17
David <david.grech26@gmail.com>
2020-11-03 12:03:02 +0000

    Add messages.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,6 +8,13 @@
     {{Form::label('title', 'Title')}}
     {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
   </div>
+  <div class="form-group">
+    {{Form::label('body', 'Body')}}
+    {{Form::textarea('body', '', ['class' => 'form-control', 'placeholder' => 'Body'])}}
+  </div>
+  {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
   {!! Form::close() !!}
-
+  
 @endsection
+
+
