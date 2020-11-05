c4db0d8ceee42e25fed7ecab9ee42142e514747d
David <david.grech26@gmail.com>
2020-11-05 17:39:13 +0000

    Edit post image

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -120,14 +120,31 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $this->validate($request,[
-          'title' => 'required',
-          'body' => 'required'
-        ]);
+      $this->validate($request,[
+        'title' => 'required',
+        'body' => 'required',
+        'cover_image' => 'image|nullable|max:1999'
+      ]);
+      //handle the file upload
+      if($request->hasFile('cover_image')){
+        //get filename with extentions
+        $filenameWithExt = $request->file('cover_image')->getClientOriginalName();
+        //get just filename
+        $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+        //get just ext
+        $extension = $request->file('cover_image')->getClientOriginalExtension();
+        //filename to store
+        $fileNameToStore = $filename .'_'.time().'.'.$extension;
+        //upload image
+        $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
+      }
 
         $post = Post::find($id);
         $post->title = $request->input('title');
         $post->body = $request->input('body');
+        if($request->hasFile('cover_image')){
+          $post->cover_image = $fileNameToStore;
+        }
         $post->save();
 
         return redirect('/posts')->with('success', 'Post updated.');
