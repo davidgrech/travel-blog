05a94366e05557722d4cd654f6a0e985f33354c3
David <david.grech26@gmail.com>
2021-01-25 11:37:44 +0000

    Update home.blade.php

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -15,7 +15,7 @@
                     @endif
 
                     <a href="/posts/create" class="btn btn-primary">Create Post</a>
-                    <h3>Your Blog Posts</h3>
+                    <h3 style="padding-top: 0.5rem">Your Blog Posts</h3>
                     @if(count($posts) > 0)
                         <table class="table table-triped">
                             <tr>
